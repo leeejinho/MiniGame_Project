81a3ae0e9c9f12842652e8a9f7c64b9705091a82
leeejinho <86703795+leeejinho@users.noreply.github.com>
2025-02-18 11:35:29 +0900

    [feat] Player 이동, 맵 배치

---
+++ Assets/Scripts/Entity/BaseController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseController : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    protected Rigidbody2D rigid;
+    protected AnimationHandler animationHandler;
+    protected StatHandler statHandler;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    protected virtual void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animationHandler = GetComponent<AnimationHandler>();
+        statHandler = GetComponent<StatHandler>();
+    }
+
+    protected virtual void Update()
+    {
+        Rotate(lookDirection);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movement(movementDirection);
+    }
+
+    private void Movement(Vector2 direction)
+    {
+        direction = direction * statHandler.Speed;
+
+        rigid.velocity = direction;
+        animationHandler.Move(direction);
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+
+        characterRenderer.flipX = isLeft;
+    }
+}
